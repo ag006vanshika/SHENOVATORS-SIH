9b1f344f21522a1ef89990a98978b0d87229f89a
ag006vanshika <ag006vanshika@gmail.com>
2025-09-07 16:23:18 +0530

    Initial setup of vite react app and tailwind css

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+//folder that contains the main entry point for the React application
+//renders the App component into the root DOM element
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
